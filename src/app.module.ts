dd8dcae5afd004a6ff39726a468d4ed6a1bb8010
niliotiii <d.saiter@estudante.ifro.edu.br>
2023-12-19 18:53:29 -0400

    refactor: exclude in not use

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CriancaModule } from './crianca/crianca.module';
 import { EnderecoModule } from './endereco/endereco.module';
 import { ContatoModule } from './contato/contato.module';
 
 @Module({
   imports: [CriancaModule, EnderecoModule, ContatoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
