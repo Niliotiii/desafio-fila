06be3e9a462cafbd418958de0e4728dabe01c376
niliotiii <d.saiter@estudante.ifro.edu.br>
2023-12-19 18:40:49 -0400

    refactor: delete module pessoa

--- src/pessoa/pessoa.provider.ts
+++
@@ -1,8 +0,0 @@
-import { DataSource } from 'typeorm';
-import { Pessoa } from './entities/pessoa.entity';
-
-export const pessoaProvider = {
-  provide: 'PESSOA_REPOSITORY',
-  useFactory: (dataSource: DataSource) => dataSource.getRepository(Pessoa),
-  inject: ['DATA_SOURCE'],
-};
